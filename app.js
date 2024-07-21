188d43deac1c879219fd2235c5f63ef2991ac417
destny69 <mzldestiny69@gmail.com>
2024-07-21 18:12:33 +0545

    bugs: fix bugs

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
-const express= require("express");
+// const express= require("express");
 
-const app = express();
+// const app = express();
+// const path = require('path');
+// const authRoutes = require('./routes/auth.routes');
+// const db =require('./data/database');
+// app.use(authRoutes);
+// app.use(express.static('public'));
+
+// app.use(express.urlencoded({extended:false}));
+// app.set('view engine','ejs');
+// app.set('views',path.join(__dirname,'views'));
+
+// db.connectToDatabase().then(function(){
+//     app.listen(3000);
+// }).catch(function(error){
+//     console.log('Failed to connect to the database');
+//     console.log(Error);
+// });
+
+const express = require('express');
 const path = require('path');
 const authRoutes = require('./routes/auth.routes');
-const db =require('./data/database');
-app.use(authRoutes);
+const db = require('./data/database');
+
+const app = express();
+
+// Middleware to parse request bodies
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files
 app.use(express.static('public'));
 
-app.use(express.urlencoded({extended:false}));
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-db.connectToDatabase().then(function(){
-    app.listen(3000);
-}).catch(function(error){
-    console.log('Failed to connect to the database');
-    console.log(Error);
+// Use routes
+app.use(authRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Connect to the database and start the server
+db.connectToDatabase().then(function() {
+  app.listen(3000, () => {
+    console.log('Server is running on http://127.0.0.1:3000');
+  });
+}).catch(function(error) {
+  console.log('Failed to connect to the database');
+  console.error(error);
 });
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,30 +1,33 @@
 const User = require('../models/user.model');
 
+function getSignup(req, res) {
+  res.render('customer/auth/signup');
+}
 
-
-function getSignup(req,res){
-    res.render('customer/auth/signup');
-    }
-
-    async function signup(req,res)
-{
- const user = new User(req.body.email,
-    req.body.password,
-    req.body.fullname,
-    req.body.street,
-    req.body.postal,
-    req.body.city);
+async function signup(req, res, next) {
+  try {
+    console.log('hello');
+    const user = new User(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    );
     await user.signup();
     res.redirect('/login');
+  } catch (err) {
+    next(err); // Pass errors to the error handling middleware
+  }
 }
 
-function getLogin(req,res){
-res.render('customer/auth/login');
+function getLogin(req, res) {
+  res.render('customer/auth/login');
 }
 
-
-module.exports={
-    getSignup:getSignup,
-    getLogin:getLogin,
-    signup:signup
-}
+module.exports = {
+  getSignup,
+  getLogin,
+  signup
+};
